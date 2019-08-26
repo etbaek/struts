1760c0ce0fd51c814d2d74bfc4f929fc59201a79
etbaek <etbaek@lsis.com>
2019-08-26 09:11:13 +0900

    struts

--- src/main/java/org/superbiz/struts/UsersController.java
+++ src/main/java/org/superbiz/struts/UsersController.java
@@ -1,10 +1,7 @@
 package org.superbiz.struts;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.ui.Model;
 import java.util.Optional;
